afd65f64662597f305a6b8c18034a6ad1c63e906
Radouane Bahi <radbahi@gmail.com>
2021-04-28 06:15:48 -0400

    added like and comment count from backend

--- graphql/resolvers/index.js
+++ graphql/resolvers/index.js
@@ -4,6 +4,11 @@ const usersResolvers = require('./users')
 const commentsResolvers = require('./comments')
 
 module.exports = {
+  Post: {
+    //remember that parent contains info from the previous step
+    likeCount: (parent) => parent.likes.length,
+    commentCount: (parent) => parent.comments.length,
+  },
   Query: {
     ...postsResolvers.Query, // spread operator? why?
   },
